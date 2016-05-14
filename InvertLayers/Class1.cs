665f1afdf2d43e95a8123ec1f42a0b7519bbd604
Eugen Mihai Dobre <Eugen Mihai Dobre>
2016-05-14 17:54:44 +0200

    The AutoCAD command to invert layers status on/off.

--- InvertLayers/Class1.cs
+++ InvertLayers/Class1.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.ApplicationServices;
 
 namespace InvertLayers
 {
     public class Class1
     {
+
+        [CommandMethod("InvertLayers")]
+        public void InvertLayers()
+        {
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acDB = acDoc.Database;
+            try
+            {
+                using (Transaction acTrans = acDB.TransactionManager.StartTransaction())
+                {
+                    LayerTable acLayTbl = (LayerTable)acTrans.GetObject(acDB.LayerTableId, OpenMode.ForRead);
+
+                    foreach (ObjectId objLayer in acLayTbl)
+                    {
+                        LayerTableRecord acLayTblRec = (LayerTableRecord)acTrans.GetObject(objLayer, OpenMode.ForWrite);
+                        //string LayerName= acLayTblRec.Name;
+                        if(acLayTblRec.IsOff)
+                        {
+                            acLayTblRec.IsOff = false;
+                        }
+                        else
+                        {
+                            acLayTblRec.IsOff = true;
+                        }
+                    }
+                    acTrans.Commit();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Application.ShowAlertDialog(ex.Message);
+
+            }
+        }
     }
 }
